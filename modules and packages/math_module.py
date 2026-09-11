60fbf2edcb07728c7fa637e3032a83125cd6827e
sahadddddd <sahad6471@gmail.com>
2026-09-11 13:41:25 +0530

    math module

---
+++ modules and packages/math_module.py
@@ -0,0 +1,11 @@
+# import math
+# print(math.sqrt(16))
+# print (math.pi)
+
+# from math import sqrt,pi
+# print(sqrt(16))
+# print(pi)
+
+import datetime as dt
+today=dt.date.today()
+print("today's date:",today)
