6bc627e7bd3997e00339b7b524584ded7c8e2e7d
sahadddddd <sahad6471@gmail.com>
2026-09-14 11:31:43 +0530

    my packages

--- modules and packages/main.py
+++ modules and packages/main.py
@@ -1,3 +1,13 @@
 import utility
+from my_package import calculator,greeter
+
 print(utility.greet('alice'))
 print(utility.add(10,30))
+
+print(greeter.say_hello("sahad"))
+
+added=calculator.add(100,200)
+print('add:',added)
+
+sub=calculator.substract(added,20)
+print('sub:',sub)
