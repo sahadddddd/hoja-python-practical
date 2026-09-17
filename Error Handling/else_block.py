d8dc12b45ba7bae67c20a340d9047d892fa7d8de
sahadddddd <sahad6471@gmail.com>
2026-09-17 14:32:16 +0530

    specifi error handling

---
+++ Error Handling/else_block.py
@@ -0,0 +1,10 @@
+try:
+    number = int(input("Enter a number: "))
+    result = 10 / number
+    print(result)
+
+except:
+    print("oops! something went wrong")
+
+else:
+    print('code run successfully!')
